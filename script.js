54239aa34e1712b51b6008112d933d59a3c9e2e4
XXDIUX <162715717+XXDIUX@users.noreply.github.com>
2024-09-04 11:34:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 const notas = [7, 8, 8, 9, 5];
 let soma = 0;
+let i = 0;
 
-for (let i = 0; i < notas.length; i++) {
+while (i < notas.length) {
     soma += notas[i];
+    i++;
 }
 
 const media = soma / notas.length;
-console.log("Média calculada com for loop:", media);
+console.log("Média calculada com while loop:", media);
